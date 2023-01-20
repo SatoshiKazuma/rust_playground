2cbf86be02badf66c4725331c3f3953de8cb8f8d
SatoshiKazuma <sairamkumarm21@gmail.com>
2023-01-20 17:24:49 +0530

    cargo did this

---
+++ hello_world/target/debug/deps/hello_world-00b4c9e72177fc72.d
@@ -0,0 +1,5 @@
+/home/sr21/Projects/VSCode/Rust/hello_world/target/debug/deps/hello_world-00b4c9e72177fc72.rmeta: src/main.rs
+
+/home/sr21/Projects/VSCode/Rust/hello_world/target/debug/deps/hello_world-00b4c9e72177fc72.d: src/main.rs
+
+src/main.rs:
---
+++ hello_world/target/debug/deps/hello_world-4d7ecab7457d8afe.d
@@ -0,0 +1,5 @@
+/home/sr21/Projects/VSCode/Rust/hello_world/target/debug/deps/hello_world-4d7ecab7457d8afe.rmeta: src/main.rs
+
+/home/sr21/Projects/VSCode/Rust/hello_world/target/debug/deps/hello_world-4d7ecab7457d8afe.d: src/main.rs
+
+src/main.rs:
